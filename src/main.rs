f83736b748798676fa9fba8ba940cfa9df37765e
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-08-01 12:51:54 +0200

    traverse the given root path and list all files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{fmt::Debug, path::PathBuf};
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, help = "TODO")]
+    path: PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+    for entry in WalkDir::new(args.path)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_file())
+    {
+        println!("{}", entry.path().display());
+    }
+    Ok(())
 }
